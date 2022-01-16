9552d4363aa4b59bbdbb75f59393e0f03be34e9d
jfgrissom <jfgrissom@gmail.com>
2022-01-16 09:53:20 -0800

    Added generator feature

--- src/commands/generate/index.ts
+++ src/commands/generate/index.ts
@@ -1,4 +1,6 @@
 import { Command } from "@oclif/core";
+import { ethers } from "ethers";
+import * as crypto from "crypto";
 
 export default class Generate extends Command {
   static description = "Generates new key pair.";
@@ -6,8 +8,12 @@ export default class Generate extends Command {
   static examples = [`$ ft generate`];
 
   async run(): Promise<void> {
-    this.log(
-      `Here is your new key pair. It will work on all EVM compatible blockchains.`
-    );
+    const id = crypto.randomBytes(32).toString("hex");
+    const privateKey = "0x" + id;
+    const wallet = new ethers.Wallet(privateKey);
+
+    this.log("This key pair will work on all EVM compatible blockchains: ");
+    this.log("Spending (private/secret): ", privateKey);
+    this.log("Address (public/sharable): " + wallet.address);
   }
 }
