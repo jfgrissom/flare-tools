91b83c7b287823a7ada10b38efb38e00574bf2c6
jfgrissom <jfgrissom@gmail.com>
2022-01-29 06:59:42 -0800

    Moved typechain output to src dir. Added Wrap Native contract abi to repo. Implemented wrapping functionality.

--- src/commands/wrap.ts
+++ src/commands/wrap.ts
@@ -1,13 +1,14 @@
 import { Command, Flags } from '@oclif/core'
 import { ethers } from 'ethers'
 import { getProvider } from '../lib/network'
+import { WNat__factory } from '../types/factories/WNat__factory'
 
 export default class Wrap extends Command {
   static description =
     'Wraps the amount of asset provided for the given account.'
 
   static examples = [
-    '<%= config.bin %> <%= command.id %> -a 0xE29FDd1b740913CB54404a1d50A3dfC40041f619 -k 0xc5e8f61d1ab959b397eecc0a37a6517b8e67a0e7cf1f4bce5591f3ed80199122 -m 10',
+    `<%= config.bin %> <%= command.id %> -a 0xE29FDd1b740913CB54404a1d50A3dfC40041f619 -k 0xc5e8f61d1ab959b397eecc0a37a6517b8e67a0e7cf1f4bce5591f3ed80199122 -m 10`,
   ]
 
   static flags = {
@@ -15,26 +16,41 @@ export default class Wrap extends Command {
       char: 'a',
       description:
         'The public address of the account holding the funds to be wrapped.',
+      required: true,
     }),
     key: Flags.string({
       char: 'k',
       description: "The public account's signing key.",
+      required: true,
     }),
-    amount: Flags.string({ char: 'm', description: 'The amount to wrap.' }),
+    amount: Flags.string({
+      char: 'm',
+      description: 'The amount to wrap.',
+      required: true,
+    }),
   }
 
-  static args = [{ name: 'account' }, { name: 'amount' }]
+  static args = [{ name: 'account' }, { name: 'key' }, { name: 'amount' }]
 
   public async run(): Promise<void> {
     const { flags } = await this.parse(Wrap)
 
-    const provider = getProvider()
-
-    if (flags.amount && flags.account) {
+    if (flags.amount && flags.account && flags.key) {
       this.log(`Wrapping ${flags.amount} for ${flags.account}`)
+
+      // Setup the items needed to sign the transaction.
+      const provider = getProvider()
+      const signer = new ethers.Wallet(flags.key, provider)
+      const contractAddress = '0x02f0826ef6aD107Cfc861152B32B52fD11BaB9ED' // This is a fixed value on the network.
+
+      // Setup the contract call.
+      const wnat = WNat__factory.connect(contractAddress, signer)
+      const wei = ethers.utils.parseEther(flags.amount)
+      const amount = wei.toString()
+      await wnat.deposit({ from: flags.account, value: amount })
     } else {
       this.log(
-        'Please provide an account number, a private key, and an amount when running this command.'
+        'Please provide an account number, a private key, and an amount when running this command. Try wrap --help for more information.'
       )
     }
   }
